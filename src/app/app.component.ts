3902c4ba91915b8375ab06a27ff2b6430eac18cc
Del Rosario Perez <hector.delrosario@atos.net>
2019-12-16 16:31:45 +0000

    se añaden librerias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WatchbedFront';
+
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBo1cqkSDB_xlro5QfG1LUybkVtcrLOwdU',
+      authDomain: 'watchbed.firebaseapp.com'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const ROUTES: Routes = [
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES);
   ],
   providers: [],
   bootstrap: [AppComponent]
